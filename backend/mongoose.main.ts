eadd26ce1ff2a0dad31f467e261bab86692cf933
sohamviradiya <sohamviradiya2003@gmail.com>
2023-05-29 21:39:56 +0530

    clean mongoose code

--- backend/mongoose.main.ts
+++ backend/mongoose.main.ts
@@ -11,11 +11,11 @@ export default async function connectDb() {
 			return "already connected";
 		}
 		if (!process.env.MONGO_URI) return "Unable to connect to MongoDB";
-		const mongooseInstance = await mongoose.connect(process.env.MONGO_URI, {});
+		await mongoose.connect(process.env.MONGO_URI, {});
 		console.log("connected");
 	} catch (error) {
 		console.log(error);
 		return error;
 	}
 	return "connected";
-};
+}
--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,16 +1,9 @@
 import { NextResponse } from "next/server";
 import connectDb from "backend/mongoose.main";
-import StockModel from "backend/models/stock.schema";
+import CompanyService from "backend/services/company.service";
+
 export async function GET() {
 	await connectDb();
-	return await StockModel.find({})
-		.exec()
-		.then((data) => {
-			console.log(data);
-			return NextResponse.json(data);
-		})
-		.catch((err) => {
-			console.log(err);
-			return NextResponse.json(err);
-		});
+	return NextResponse.json({
+	});
 }
