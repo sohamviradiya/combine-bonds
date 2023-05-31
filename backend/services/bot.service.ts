83a4511c0dbb2f213f3cb97316e9b3e7044663f3
sohamviradiya <sohamviradiya2003@gmail.com>
2023-05-31 22:45:47 +0530

    add bot service

--- backend/services/bot.service.ts
+++ backend/services/bot.service.ts
@@ -1,7 +1,23 @@
-import BotInterface, { BOT_CLASS } from "backend/interfaces/bot.interface";
+import BotInterface from "backend/interfaces/bot.interface";
+import BotModel from "backend/models/bot.schema";
 
 const BotService = (() => {
-	
+	const addBot = async (bot: BotInterface) => {
+		return await new BotModel({
+			...bot,
+		}).save();
+	};
+	const getBots = async () => {
+		return await BotModel.find({}).exec();
+	};
+	const getBot = async (bot_id: string) => {
+		return await BotModel.findById(bot_id).exec();
+     };
+     return {
+          addBot,
+          getBots,
+          getBot,
+     };
 })();
 
 export default BotService;
--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,8 +1,14 @@
 import { NextResponse } from "next/server";
 import connectDb from "backend/mongoose.main";
-
+import CompanyGenerator from "backend/generators/company.generator";
+import StockGenerator from "backend/generators/stocks.generator";
+import AgencyGenerator from "backend/generators/agency.generator";
+import CompanyService from "backend/services/company.service";
+import StockService from "backend/services/stock.service";
+import AgencyService from "backend/services/agency.service";
 export async function GET() {
 	await connectDb();
 	return NextResponse.json({
+		
 	});
 }
