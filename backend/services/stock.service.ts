58d8fed3ecf5429e6d8fc448e6b41ff21315021d
sohamviradiya <sohamviradiya2003@gmail.com>
2023-06-02 23:10:35 +0530

    do minor changes

--- backend/services/stock.service.ts
+++ backend/services/stock.service.ts
@@ -7,17 +7,11 @@ import {
 	StockInterfaceWithID,
 } from "backend/interfaces/stock.interface";
 import CompanyModel from "backend/models/company.schema";
-
 const StockService = (() => {
 	const addStock = async (stock: createStockDto) => {
-		const newStock = {
-			...stock,
-			createdAt: new Date(),
-		} as StockInterface;
+		const newStock = { ...stock, createdAt: new Date() } as StockInterface;
 		const newStockDoc = await new StockModel({ ...newStock }).save();
-		await CompanyModel.findByIdAndUpdate(stock.company, {
-			$push: { stocks: newStockDoc._id },
-		}).exec();
+		await CompanyModel.findByIdAndUpdate(stock.company, { $push: { stocks: newStockDoc._id } }).exec();
 		return newStockDoc;
 	};
 	const getStocks = async () => {
@@ -33,29 +27,36 @@ const StockService = (() => {
 	};
 	const getStock = async (_id: string): Promise<StockInterfaceWithID> => {
 		const data = await StockModel.findById(_id).exec();
-		return {
-			...data._doc,
-			price: data.price,
-			slope: data.slope,
-			double_slope: data.double_slope,
-		};
+		return { ...data._doc, price: data.price, slope: data.slope, double_slope: data.double_slope };
 	};
-
+	const changeVolume = async (_id: string, change: number) => {
+		const stock: StockInterfaceWithID = await StockModel.findById(_id).exec();
+		if (!stock) return null;
+		stock.timeline[stock.timeline.length - 1].volume_in_market += change;
+		return StockModel.findByIdAndUpdate(_id, stock).exec();
+	};
 	const addPoint = async (_id: string, valuePoint: ValuePoint) => {
 		const stock: StockInterfaceWithID = await StockModel.findById(_id).exec();
 		if (!stock) return null;
-		stock.timeline = stock.timeline.filter(
-			(point) => point.date < valuePoint.date - 100
-		);
+		stock.timeline = stock.timeline.filter((point) => point.date > valuePoint.date - 100);
 		stock.timeline.push(valuePoint);
 		return await StockModel.findByIdAndUpdate(_id, stock).exec();
 	};
-	return {
-		addStock,
-		getStocks,
-		getStock,
-		addPoint,
-	};
-})();
+	const getRandomStocks = async (count: number) => {
+		const stocks = await StockModel.find({}, { _id: 1 }).limit(count).exec();
+		return stocks.map((stock) => stock._id);
+	}
+	const getHighSlopeStocks = async (count: number) => {
+		const stocks = await StockModel.find({}, { _id: 1 }).sort({ slope: -1 }).limit(count).exec();
+		return stocks.map((stock) => stock._id);
+	}
+	const getHighDoubleSlopeStocks = async (count: number) => {
+		const stocks = await StockModel.find({}, { _id: 1 }).sort({ double_slope: -1 }).limit(count).exec();
+		return stocks.map((stock) => stock._id);
+	}
 
+	return { changeVolume, addStock, getStocks, getStock, addPoint, getRandomStocks, getHighSlopeStocks, getHighDoubleSlopeStocks };
+})();
 export default StockService;
+
+
--- backend/services/transaction.service.ts
+++ backend/services/transaction.service.ts
@@ -1,72 +1,43 @@
-import PortfolioInterface, {
-	Transaction,
-} from "backend/interfaces/portfolio.interface";
+import PortfolioInterface, { Transaction } from "backend/interfaces/portfolio.interface";
 import StockModel from "backend/models/stock.schema";
-
+import StockService from "./stock.service";
 const TransactionService = (() => {
-	const buyStock = async (
-		portfolio: PortfolioInterface,
-		transaction: Transaction
-	): Promise<PortfolioInterface> => {
+	const buyStock = async (portfolio: PortfolioInterface, transaction: Transaction): Promise<PortfolioInterface> => {
 		if (!transaction.stock) throw new Error("Stock not found");
-		if (portfolio.currentBalance < transaction.amount)
-			throw new Error("Insufficient funds");
-		
+		if (portfolio.currentBalance < transaction.amount) throw new Error("Insufficient funds");
 		const stockIndex = portfolio.investments.findIndex((investment) => investment.stock === transaction.stock);
-		const stock_price = await StockModel.findById(transaction.stock, {
-				price: 1,
-          }).exec();
-          if (stockIndex === -1) {
+		const stock_price = await StockModel.findById(transaction.stock, { price: 1 }).exec();
+		await StockService.changeVolume(transaction.stock, transaction.amount);
+		if (stockIndex === -1) {
 			const stock_quantity = transaction.amount / stock_price.price;
-			portfolio.investments.push({stock: transaction.stock,quantity: stock_quantity });
+			portfolio.investments.push({ stock: transaction.stock, quantity: stock_quantity });
 		} else {
-			portfolio.investments[stockIndex].quantity += Math.round(
-				transaction.amount / stock_price.price
-			);
-          }
-          portfolio.currentBalance -= transaction.amount;
+			portfolio.investments[stockIndex].quantity += Math.round(transaction.amount / stock_price.price);
+		}
+		portfolio.currentBalance -= transaction.amount;
 		return portfolio;
-     };
-     
-     const sellStock = async (
-          portfolio: PortfolioInterface,
-          transaction: Transaction
-     ): Promise<PortfolioInterface> => {
-          if (!transaction.stock) throw new Error("Stock not found");
-          const stockIndex = portfolio.investments.findIndex((investment) => investment.stock === transaction.stock);
-          if (stockIndex === -1) throw new Error("Stock not found in portfolio");
-          const stock_price = await StockModel.findById(transaction.stock, {
-               price: 1,
-          }).exec();
-          const stock_quantity = transaction.amount / stock_price.price;
-          if (portfolio.investments[stockIndex].quantity < stock_quantity) throw new Error("Insufficient stocks");
-          portfolio.investments[stockIndex].quantity -= stock_quantity;
-          portfolio.currentBalance += transaction.amount;
-          return portfolio;
-     };
-
-     const deposit = (
-          portfolio: PortfolioInterface,
-          transaction: Transaction
-     ): PortfolioInterface => {
-          portfolio.currentBalance += transaction.amount;
-          return portfolio;
-     };
-
-     const withdraw = (
-          portfolio: PortfolioInterface,
-          transaction: Transaction
-     ): PortfolioInterface => {
-          if (portfolio.currentBalance < transaction.amount) throw new Error("Insufficient funds");
-          portfolio.currentBalance -= transaction.amount;
-          return portfolio;
-     };
-     return {
-          buyStock,
-          sellStock,
-          deposit,
-          withdraw,
-     };
+	};
+	const sellStock = async (portfolio: PortfolioInterface, transaction: Transaction): Promise<PortfolioInterface> => {
+		if (!transaction.stock) throw new Error("Stock not found");
+		const stockIndex = portfolio.investments.findIndex((investment) => investment.stock === transaction.stock);
+		if (stockIndex === -1) throw new Error("Stock not found in portfolio");
+		const stock_price = await StockModel.findById(transaction.stock, { price: 1 }).exec();
+		await StockService.changeVolume(transaction.stock, transaction.amount);
+		const stock_quantity = transaction.amount / stock_price.price;
+		if (portfolio.investments[stockIndex].quantity < stock_quantity) throw new Error("Insufficient stocks");
+		portfolio.investments[stockIndex].quantity -= stock_quantity;
+		portfolio.currentBalance += transaction.amount;
+		return portfolio;
+	};
+	const deposit = (portfolio: PortfolioInterface, transaction: Transaction): PortfolioInterface => {
+		portfolio.currentBalance += transaction.amount;
+		return portfolio;
+	};
+	const withdraw = (portfolio: PortfolioInterface, transaction: Transaction): PortfolioInterface => {
+		if (portfolio.currentBalance < transaction.amount) throw new Error("Insufficient funds");
+		portfolio.currentBalance -= transaction.amount;
+		return portfolio;
+	};
+	return { buyStock, sellStock, deposit, withdraw };
 })();
-
 export default TransactionService;
