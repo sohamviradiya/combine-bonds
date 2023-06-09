8490b72d9dc633913e7ca9b3b0449042ce8be5f5
sohamviradiya <sohamviradiya2003@gmail.com>
2023-06-09 23:14:24 +0530

    add stock dumping service

--- backend/services/transaction.service.ts
+++ backend/services/transaction.service.ts
@@ -1,4 +1,4 @@
-import PortfolioInterface, { Transaction } from "backend/interfaces/portfolio.interface";
+import PortfolioInterface, { PortfolioInterfaceWithID, Transaction } from "backend/interfaces/portfolio.interface";
 import { StockInterfaceWithID } from "backend/interfaces/stock.interface";
 import StockModel from "backend/models/stock.schema";
 const TransactionService = (() => {
@@ -7,8 +7,7 @@ const TransactionService = (() => {
 		portfolio: PortfolioInterface,
 		transaction: Transaction
 	): Promise<PortfolioInterface> => {
-		if (transaction.class != "STOCK PURCHASE" && transaction.class != "STOCK SALE")
-			throw new Error("Invalid transaction class");
+		if (transaction.class != "STOCK PURCHASE") throw new Error("Invalid transaction class");
 		if (!transaction.stock) throw new Error("Stock not found");
 		const data = await StockModel.findById(transaction.stock).exec();
 		const stock = {
@@ -39,8 +38,7 @@ const TransactionService = (() => {
 		portfolio: PortfolioInterface,
 		transaction: Transaction
 	): Promise<PortfolioInterface> => {
-		if (transaction.class != "STOCK PURCHASE" && transaction.class != "STOCK SALE")
-			throw new Error("Invalid transaction class");
+		if (transaction.class != "STOCK SALE") throw new Error("Invalid transaction class");
 		if (!transaction.stock) throw new Error("Stock not found");
 		const data = await StockModel.findById(transaction.stock).exec();
 		const stock = {
@@ -57,21 +55,35 @@ const TransactionService = (() => {
 
 		if (portfolio.investments[stockIndex].quantity < stock_quantity) throw new Error("Insufficient stocks");
 
-		// Adjust Balance and quantity
 		portfolio.currentBalance += transaction.amount;
 		portfolio.investments[stockIndex].quantity -= stock_quantity;
-
-		// Remove stock from portfolio if quantity is less than 0.1
-		if (portfolio.investments[stockIndex].quantity < 0.25) {
-			portfolio.currentBalance += portfolio.investments[stockIndex].quantity * stock.price;
-			stock.timeline[stock.timeline.length - 1].volume_in_market -= portfolio.investments[stockIndex].quantity;
-			portfolio.investments.splice(stockIndex, 1);
-			stock.traders = stock.traders.filter((trader) => trader != id);
-		}
 
 		await StockModel.findByIdAndUpdate(transaction.stock, stock);
 		return portfolio;
 	};
+
+	const dumpStock = async (portfolio: PortfolioInterfaceWithID, stock_id: string) => {
+		const stockIndex = portfolio.investments.findIndex((investment) => investment.stock == stock_id);
+
+		if (stockIndex === -1) throw new Error("Stock not found in portfolio");
+
+		const stock_quantity = portfolio.investments[stockIndex].quantity;
+		const data = await StockModel.findById(stock_id).exec();
+		const stock = {
+			...data._doc,
+			price: data.price,
+		} as StockInterfaceWithID;
+		stock.timeline[stock.timeline.length - 1].volume_in_market -= stock_quantity;
+		const amount = stock_quantity * stock.price;
+
+		portfolio.currentBalance += amount;
+		portfolio.investments.splice(stockIndex, 1);
+		stock.traders = stock.traders.filter((trader) => trader != portfolio._id);
+
+		await StockModel.findByIdAndUpdate(stock_id, stock);
+		return portfolio;
+	};
+
 	const deposit = (portfolio: PortfolioInterface, transaction: Transaction): PortfolioInterface => {
 		portfolio.currentBalance += transaction.amount;
 		portfolio.netWorth[portfolio.netWorth.length - 1].value += transaction.amount;
@@ -83,6 +95,6 @@ const TransactionService = (() => {
 		portfolio.netWorth[portfolio.netWorth.length - 1].value -= transaction.amount;
 		return portfolio;
 	};
-	return { buyStock, sellStock, deposit, withdraw };
+	return { buyStock, sellStock, deposit, withdraw, dumpStock };
 })();
 export default TransactionService;
