a1532799287e146335945792db1a89704904c330
sohamviradiya <sohamviradiya2003@gmail.com>
2023-06-19 18:08:18 +0530

    clean route file

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,7 +1,9 @@
 import { NextResponse } from "next/server";
 import invokeMain from "@/server/tasks/main";
+import connectDb from "@/server/mongoose.main";
 let k = 0;
 export async function GET() {
-	await invokeMain();
+	// await invokeMain();
+	await connectDb();
 	return NextResponse.json({ message: "Done" });
 }
