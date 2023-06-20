1287d60abdb14cdc8cd8f44474b4167f69a4e649
sohamviradiya <sohamviradiya2003@gmail.com>
2023-06-20 21:36:34 +0530

    add transaction route

---
+++ src/app/api/transaction/route.ts
@@ -0,0 +1,9 @@
+import PortfolioService from "@/server/services/portfolio.service";
+import { Transaction } from "@/server/types/portfolio.interface";
+import { NextResponse } from "next/server";
+export async function POST(request: Request) {
+	const body = await request.json();
+	const { _id, transaction }: { _id: string; transaction: Transaction } = body;
+	await PortfolioService.performTransactions(_id, [transaction]);
+	return NextResponse.json({ message: "Done" });
+}
