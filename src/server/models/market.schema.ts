ad5952e5d286df3ec205ce74735b077cf97597e5
sohamviradiya <sohamviradiya2003@gmail.com>
2023-08-03 22:14:07 +0530

    change virtuals structure

--- src/server/models/market.schema.ts
+++ src/server/models/market.schema.ts
@@ -1,18 +1,18 @@
 import mongoose, { Schema } from "mongoose";
 
 const MarketSchema = new Schema({
-	date: {
-		type: Schema.Types.Number,
-		required: true,
-	},
-	cumulative_market_capitalization: {
-		type: Schema.Types.Number,
-		required: true,
-	},
-	cumulative_net_worth: {
-		type: Schema.Types.Number,
-		required: true,
-	},
+    date: {
+        type: Schema.Types.Number,
+        required: true,
+    },
+    cumulative_market_capitalization: {
+        type: Schema.Types.Number,
+        required: true,
+    },
+    cumulative_net_worth: {
+        type: Schema.Types.Number,
+        required: true,
+    },
 }, { toJSON: { virtuals: true } });
 
 const MarketModel = mongoose.models["Market"] ?? mongoose.model("Market", MarketSchema);
--- src/server/models/stock.schema.ts
+++ src/server/models/stock.schema.ts
@@ -1,52 +1,52 @@
 import {
     COMPANY_FIELDS,
-	COMPANY_FORMS,
-	STOCK_CLASS,
-	ValuePoint,
+    COMPANY_FORMS,
+    STOCK_CLASS,
+    ValuePoint,
 } from "types/stock.interface";
 import mongoose, { Schema } from "mongoose";
 
 const stockSchema = new Schema(
-	{
-		name: {
-			type: Schema.Types.String,
-			required: true,
-		},
-		gross_volume: {
-			type: Schema.Types.Number,
-			required: true,
-		},
-		timeline: {
-			type: [
-				{
-					date: {
-						type: Schema.Types.Number,
-						required: true,
-					},
-					market_valuation: {
-						type: Schema.Types.Number,
-						required: true,
-					},
-					volume_in_market: {
-						type: Schema.Types.Number,
-						required: true,
-					},
-					dividend: {
-						type: Schema.Types.Number,
-						required: true,
-					},
-				},
-			],
-		},
-		class: {
-			type: Schema.Types.String,
-			required: true,
-			enum: Object.values(STOCK_CLASS),
-		},
-		createdAt: {
-			type: Schema.Types.Date,
-			required: false,
-		},
+    {
+        name: {
+            type: Schema.Types.String,
+            required: true,
+        },
+        gross_volume: {
+            type: Schema.Types.Number,
+            required: true,
+        },
+        timeline: {
+            type: [
+                {
+                    date: {
+                        type: Schema.Types.Number,
+                        required: true,
+                    },
+                    market_valuation: {
+                        type: Schema.Types.Number,
+                        required: true,
+                    },
+                    volume_in_market: {
+                        type: Schema.Types.Number,
+                        required: true,
+                    },
+                    dividend: {
+                        type: Schema.Types.Number,
+                        required: true,
+                    },
+                },
+            ],
+        },
+        class: {
+            type: Schema.Types.String,
+            required: true,
+            enum: Object.values(STOCK_CLASS),
+        },
+        createdAt: {
+            type: Schema.Types.Date,
+            required: false,
+        },
         company: {
             name: {
                 type: Schema.Types.String,
@@ -82,88 +82,76 @@ const stockSchema = new Schema(
                 type: Schema.Types.Number,
                 required: false,
             }
-		},
-		traders: {
-			type: [
-				{
-					type: Schema.Types.ObjectId,
-					ref: "Portfolio",
-					required: true,
-				},
-			],
-		},
-	},
-	{ toJSON: { virtuals: true } }
+        },
+        traders: {
+            type: [
+                {
+                    type: Schema.Types.ObjectId,
+                    ref: "Portfolio",
+                    required: true,
+                },
+            ],
+        },
+    },
+    { toJSON: { virtuals: true } }
 );
 
 stockSchema.virtual("price").get(function (this: any) {
-	if (this.timeline.length < 1) return 0;
-	const last_point: ValuePoint = this.timeline[this.timeline.length - 1];
-	return Number(last_point.market_valuation) / this.gross_volume;
+    if (this.timeline.length < 1) return 0;
+    const last_point: ValuePoint = this.timeline[this.timeline.length - 1];
+    return last_point.market_valuation / this.gross_volume;
 });
 
 stockSchema.virtual("slope").get(function (this: any) {
-	if (this.timeline.length < 2) return 0;
-	const last_point: ValuePoint = this.timeline[this.timeline.length - 1];
-	const second_last_point: ValuePoint = this.timeline[this.timeline.length - 2];
-	return (
-		(Number(last_point.market_valuation) -
-			Number(second_last_point.market_valuation)) /
-		Number(second_last_point.market_valuation)
-	);
+    if (this.timeline.length < 2) return 0;
+    const last_point: ValuePoint = this.timeline[this.timeline.length - 1];
+    const second_last_point: ValuePoint = this.timeline[this.timeline.length - 2];
+    const diff = last_point.market_valuation - second_last_point.market_valuation;
+    return diff / second_last_point.market_valuation;
 });
 
 stockSchema.virtual("double_slope").get(function (this: any) {
-	if (this.timeline.length < 3) return 0;
-	const last_point: ValuePoint = this.timeline[this.timeline.length - 1];
-	const second_last_point: ValuePoint = this.timeline[this.timeline.length - 2];
-	const third_last_point: ValuePoint = this.timeline[this.timeline.length - 3];
-	const last_slope =
-		(Number(last_point.market_valuation) -
-			Number(second_last_point.market_valuation)) /
-		Number(second_last_point.market_valuation);
-	const second_last_slope =
-		(Number(second_last_point.market_valuation) -
-			Number(third_last_point.market_valuation)) /
-		Number(third_last_point.market_valuation);
-	return (last_slope - second_last_slope) / second_last_slope;
+    if (this.timeline.length < 3) return 0;
+    const last_point: ValuePoint = this.timeline[this.timeline.length - 1];
+    const second_last_point: ValuePoint = this.timeline[this.timeline.length - 2];
+    const last_diff = last_point.market_valuation - second_last_point.market_valuation;
+
+    const last_slope = last_diff / second_last_point.market_valuation;
+
+    const third_last_point: ValuePoint = this.timeline[this.timeline.length - 3];
+    const second_last_diff = second_last_point.market_valuation - third_last_point.market_valuation;
+    const second_last_slope = second_last_diff / third_last_point.market_valuation;
+    const slope_diff = last_slope - second_last_slope;
+    return slope_diff / second_last_slope;
 });
 
 stockSchema.virtual("fall_since_peak").get(function (this: any) {
-	if (this.timeline.length < 2) return 0;
-	const latest_market_valuation =
-		this.timeline[this.timeline.length - 1].market_valuation;
-	let k = this.timeline.length - 2;
-	while (
-		k >= 0 &&
-		this.timeline[k].market_valuation >= this.timeline[k + 1].market_valuation
-	)
-		k--;
-	return (
-		(Number(this.timeline[k + 1].market_valuation) -
-			Number(latest_market_valuation)) /
-		Number(this.timeline[k + 1].market_valuation)
-	);
+    if (this.timeline.length < 2) return 0;
+    const latest_market_valuation =
+        this.timeline[this.timeline.length - 1].market_valuation;
+    let k = this.timeline.length - 2;
+    while (
+        k >= 0 &&
+        this.timeline[k].market_valuation >= this.timeline[k + 1].market_valuation
+    ) k--;
+    const diff = latest_market_valuation - this.timeline[k + 1].market_valuation;
+    return -diff / this.timeline[k + 1].market_valuation;
 });
 
 stockSchema.virtual("rise_since_trough").get(function (this: any) {
-	if (this.timeline.length < 2) return 0;
-	const latest_market_valuation =
-		this.timeline[this.timeline.length - 1].market_valuation;
-	let k = this.timeline.length - 2;
-	while (
-		k >= 0 &&
-		this.timeline[k].market_valuation <= this.timeline[k + 1].market_valuation
-	)
-		k--;
-	return (
-		(Number(latest_market_valuation) -
-			Number(this.timeline[k + 1].market_valuation)) /
-		Number(this.timeline[k + 1].market_valuation)
-	);
+    if (this.timeline.length < 2) return 0;
+    const latest_market_valuation =
+        this.timeline[this.timeline.length - 1].market_valuation;
+    let k = this.timeline.length - 2;
+    while (
+        k >= 0 &&
+        this.timeline[k].market_valuation <= this.timeline[k + 1].market_valuation
+    )
+        k--;
+    const diff = latest_market_valuation - this.timeline[k + 1].market_valuation;
+    return diff / this.timeline[k + 1].market_valuation;
 });
 
-const StockModel =
-	mongoose.models["Stock"] ?? mongoose.model("Stock", stockSchema);
+const StockModel = mongoose.models["Stock"] ?? mongoose.model("Stock", stockSchema);
 
 export default StockModel;
