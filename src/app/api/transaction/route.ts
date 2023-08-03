4400cffa912ff301842b7744db9b2223b5cd4862
sohamviradiya <sohamviradiya2003@gmail.com>
2023-08-03 10:19:09 +0530

    remove frontend

--- src/app/api/transaction/route.ts
+++
@@ -1,9 +0,0 @@
-import PortfolioService from "@/server/services/portfolio.service";
-import { Transaction } from "types/portfolio.interface";
-import { NextResponse } from "next/server";
-export async function POST(request: Request) {
-	const body = await request.json();
-	const { id, transaction }: { id: string; transaction: Transaction } = body;
-	await PortfolioService.performTransactions(id, [transaction]);
-	return NextResponse.json({ message: "Done" });
-}
