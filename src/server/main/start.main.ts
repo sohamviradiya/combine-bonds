ec8b1e75c4cf93324b40e17eb56ef6ff7c0bcba5
sohamviradiya <sohamviradiya2003@gmail.com>
2023-09-08 14:09:49 +0530

    resolve date mismatches

--- src/server/main/start.main.ts
+++ src/server/main/start.main.ts
@@ -2,7 +2,7 @@ import { CronJob } from "cron";
 
 import MainRun from "@/server/main/run.main";
 import connectDb from "@/server/main/mongoose.main";
-import generateMain from "@/server/main/seed.main";
+import seedMain from "@/server/main/seed.main";
 
 import { getAllAgencies } from "@/server/services/agency.service";
 import { getAllBots } from "@/server/services/bot.service";
@@ -24,17 +24,21 @@ export default async function MainStart() {
     await connectDb();
     f = true;
     agencies = await getAllAgencies();
+    
     if (agencies.length === 0) {
-        await generateMain();
+        await seedMain();
+        date = 0;
         agencies = await getAllAgencies();
     }
+    else
+        date = await getDate() + 1;
     portfolios = await getAllPortfolios();
     bots = await getAllBots();
     stocks = await getAllStocks();
-    date = await getDate() + 1;
     const job = new CronJob(`*/${SLOT_DURATION} * * * *`, async () => {
         console.log("Running cron job", date);
-        await MainRun(agencies, portfolios, bots, stocks, date++);
+        await MainRun(agencies, portfolios, bots, stocks, date);
+        date++;
     }, null, true, "America/New_York");
     job.start();
     console.log("Starting cron job");
