c57ae0a1342668edfbc964f087715f9b40f46e58
sohamviradiya <sohamviradiya2003@gmail.com>
2023-09-08 15:17:58 +0530

    adjust params

--- src/server/global.config.ts
+++ src/server/global.config.ts
@@ -1,10 +1,10 @@
 
-export const AGENCY_PRICE_INCREMENT = 0.04;
+export const AGENCY_PRICE_INCREMENT = 0.06;
 
-export const BOT_MIN_INVESTMENT = 0.01;
-export const BOT_INVESTMENT_MULTIPLIER = 2;
-export const BOT_LOSS_AVERSION = 0.1;
-export const BOT_STOCK_CLEARANCE = 0.4;
+export const BOT_MIN_INVESTMENT = 0.02;
+export const BOT_INVESTMENT_MULTIPLIER = 3;
+export const BOT_LOSS_AVERSION = 0.2;
+export const BOT_STOCK_CLEARANCE = 0.3;
 
 export const PORTFOLIO_MINIMUM_BALANCE = 10000;
 export const PORTFOLIO_STARTING_BALANCE = 100000;
@@ -16,5 +16,5 @@ export const MARKET_BASE = 1000;
 export const DATE_LIMIT = 20;
 export const SLOT_DURATION = 1;
 
-export const DIVIDEND_FACTOR = 5;
+export const DIVIDEND_FACTOR = 8;
 
