a2222ba3784d013ee888269513fba75ab733269b
sohamviradiya <sohamviradiya2003@gmail.com>
2023-09-09 22:57:24 +0530

    add global context

--- src/context/global.tsx
+++ src/context/global.tsx
@@ -1,11 +1,17 @@
 "use client";
 
+import MaskContext from "./mask";
+import QueryContext from "./query";
 import ThemeContext from "./theme";
 
 export default function MainContext({ children, }: { children: React.ReactNode }) {
     return (
-        <ThemeContext>
-            {children}
-        </ThemeContext>
+        <MaskContext>
+            <ThemeContext>
+                <QueryContext>
+                    {children}
+                </QueryContext>
+            </ThemeContext>
+        </MaskContext >
     )
 }
