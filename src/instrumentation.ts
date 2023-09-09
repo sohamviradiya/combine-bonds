15811687bc10070a2f787b47c62cc35e14e59137
sohamviradiya <sohamviradiya2003@gmail.com>
2023-09-09 23:34:08 +0530

    replace data-generator with db-connecter

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,7 +1,9 @@
 import MainStart from "@/server/main/start.main";
+import MongooseConnect from "./server/main/mongoose.main";
 
 export async function register() {
     console.log("Registering instrumentation at " + new Date().toUTCString());
-    await MainStart();
+    // await MainStart();
+    await MongooseConnect();
     console.log("Instrumentation registered at " + new Date().toUTCString());
 }
