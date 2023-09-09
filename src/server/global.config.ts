30417b3341f6701ae49f67d8ea1465931c974ce2
sohamviradiya <sohamviradiya2003@gmail.com>
2023-09-09 18:40:56 +0530

    adjust config for data generations

--- src/server/global.config.ts
+++ src/server/global.config.ts
@@ -1,8 +1,8 @@
 
-export const AGENCY_PRICE_INCREMENT = 0.06;
+export const AGENCY_PRICE_INCREMENT = 0.02;
 
 export const BOT_MIN_INVESTMENT = 0.02;
-export const BOT_INVESTMENT_MULTIPLIER = 3;
+export const BOT_INVESTMENT_MULTIPLIER = 4;
 export const BOT_LOSS_AVERSION = 0.2;
 export const BOT_STOCK_CLEARANCE = 0.3;
 
@@ -13,8 +13,11 @@ export const STOCK_DUMP_THRESHOLD = 10;
 export const DEFAULT_MARKET_SENTIENCE_INDEX = 1000;
 
 export const MARKET_BASE = 1000;
-export const DATE_LIMIT = 20;
+export const DATE_LIMIT = 30;
 export const SLOT_DURATION = 1;
 
 export const DIVIDEND_FACTOR = 8;
+
+export const NUM_OF_STOCKS = 50;
+export const NUM_OF_PORTFOLIOS = 60;
 
