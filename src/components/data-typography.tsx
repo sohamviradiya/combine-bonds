7dcf2d57f562fc92d89df363ec2dec01ec71d62c
sohamviradiya <sohamviradiya2003@gmail.com>
2023-09-10 02:15:08 +0530

    set special typography for data

--- src/components/data-typography.tsx
+++ src/components/data-typography.tsx
@@ -1,7 +1,7 @@
 import { Typography } from '@mui/material';
 
-export default function DataTypography({ value, unit, sign }: { value: number; unit: string; sign?: boolean }) {
+export default function DataTypography({ value, unit, sign, label }: { value: number; unit: string; sign?: boolean, label?: string }) {
     return (<Typography variant="h5" sx={{
         color: value >= 0 ? 'success.main' : 'error.main',
-    }}>{` ${sign ? (value >= 0 ? '+' : '-') : ''} `} {Math.abs(value).toFixed(2)} {unit}</Typography>);
+    }}>{(label)} {` ${sign ? (value >= 0 ? '+' : '-') : ''} `} {Math.abs(value).toFixed(2)} {unit}</Typography>);
 };
