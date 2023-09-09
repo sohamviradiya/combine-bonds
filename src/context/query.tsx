f77eb44234b59e8f73caba9c8eb27fbaf2eb4f70
sohamviradiya <sohamviradiya2003@gmail.com>
2023-09-09 22:57:08 +0530

    set up query

---
+++ src/context/query.tsx
@@ -0,0 +1,12 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
+
+export default function QueryContext({ children }: { children: React.ReactNode }) {
+
+    return (
+        <QueryClientProvider client={queryClient}>
+        {children}
+        </QueryClientProvider>
+    );
+};
