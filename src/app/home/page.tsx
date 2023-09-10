bb7f58e9dca72ad55c92c704fae4713c3483247d
sohamviradiya <sohamviradiya2003@gmail.com>
2023-09-10 13:44:29 +0530

    add mid section in the home page

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,6 +1,6 @@
 "use client";
-
-import { Container, Typography } from '@mui/material';
+import { Card, CardContent, Container, Paper, Typography } from '@mui/material';
+import Link from 'next/link';
 import background from '/public/main-background.svg';
 import Background from '@/components/background';
 import MarketAnalyticsComponent from './market-analytics';
@@ -8,35 +8,72 @@ import MarketAnalyticsComponent from './market-analytics';
 export default function Home() {
     return (
         <>
-            <Container maxWidth="xl" sx={{
-                backgroundSize: 'cover',
-                backgroundPosition: 'center',
-                position: 'relative',
-            }}>
+            <Container
+                maxWidth="xl"
+                sx={{
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'center',
+                    position: 'relative',
+                    padding: '16px', // Add some padding to create space between papers
+                }}
+            >
                 <Background src={background} />
-                <section>
+
+                <Paper
+                    elevation={3}
+                    sx={{
+                        padding: '16px',
+                        marginBottom: '16px',
+                    }}
+                >
                     <Typography variant="h1" gutterBottom>
                         Combine-Bonds
                     </Typography>
                     <Typography variant="h2" gutterBottom>
                         Stock Market Simulation
                     </Typography>
-                </section>
-                <section>
+                </Paper>
+
+                <Paper
+                    elevation={3}
+                    sx={{
+                        padding: '16px',
+                        marginBottom: '16px',
+                    }}
+                >
                     <Typography variant="h4" gutterBottom>
-                        User Offers and Insights
+                        Explore the Market: Invest in 60+ stock options
+                    </Typography>
+                    <Typography variant="h4" gutterBottom>
+                        Explore our <HomeLink text="Analytics" link="/analysis" color="info.main" /> Page
+                    </Typography>
+                    <Typography variant="h4" gutterBottom>
+                        Explore our <HomeLink text="Stocks" link="/stocks" color="success.main" /> Page
+                    </Typography>
+                </Paper>
+
+                <Paper
+                    elevation={3}
+                    sx={{
+                        padding: '16px',
+                        marginBottom: '16px',
+                    }}
+                >
+                    <Typography variant="h4" gutterBottom>
+                        Current Market Situation:
                     </Typography>
                     <MarketAnalyticsComponent />
-                </section>
-                <section>
-                    <Typography variant="h4" gutterBottom>
-                        About the Simulation
-                    </Typography>
-                    {/* Add your content for the third section here */}
-                </section>
+                </Paper>
             </Container>
         </>
     );
 }
 
 
+function HomeLink({ text, link, color }: { text: string, link: string, color: string }) {
+    return (
+        <Typography variant="h4" component="span" sx={{ color }}>
+            <Link href={link}> {text} </Link>
+        </Typography>
+    );
+};
