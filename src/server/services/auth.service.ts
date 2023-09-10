66d6742f61829bbb473918ee281a190c5d7a0797
sohamviradiya <sohamviradiya2003@gmail.com>
2023-09-10 22:43:37 +0530

    encrypt auth service

--- src/server/services/auth.service.ts
+++ src/server/services/auth.service.ts
@@ -1,8 +1,25 @@
 import SessionModel from "@/server/models/session.schema";
 import { verifyIDPassword } from "@/server/services/portfolio.service";
-import { SessionInterface } from "@/types/session.interface";
+import { SessionInterface, SessionInterfaceWithId } from "@/types/session.interface";
+import crypto from "crypto";
 
-export async function createSession(name: string, password: string) {
+const SECRET_KEY = process.env.SESSION_SECRET_KEY || "khandar-estrada-khandos-indactu";
+
+function encrypt(text: string) {
+    const cipher = crypto.createCipheriv('aes-256-cbc', Buffer.from(SECRET_KEY), crypto.randomBytes(16));
+    let encrypted = cipher.update(text, 'utf-8', 'hex');
+    encrypted += cipher.final('hex');
+    return encrypted;
+}
+
+function decrypt(text: string) {
+    const decipher = crypto.createDecipheriv('aes-256-cbc', Buffer.from(SECRET_KEY), crypto.randomBytes(16));
+    let decrypted = decipher.update(text, 'hex', 'utf-8');
+    decrypted += decipher.final('utf-8');
+    return decrypted;
+}
+
+export async function addSession(name: string, password: string) {
     const { portfolio, message } = await verifyIDPassword(name, password);
     if (!portfolio) { return { session: null, message }; }
     const expiration = new Date();
@@ -11,11 +28,30 @@ export async function createSession(name: string, password: string) {
         portfolio,
         expiration,
     });
-    const session = await newSession.save() as SessionInterface;
+    const session = await newSession.save() as SessionInterfaceWithId;
+    const encryptedSessionID = encrypt(session._id);
+    return {
+        message, session: {
+            ...session,
+            _id: encryptedSessionID,
+        }
+    }
+};
 
-    return { session, message };
-}
+export async function getSessionById(encryptedSessionID: string) {
+    const session_id = decrypt(encryptedSessionID);
+    const session = await SessionModel.findById(session_id).exec() as SessionInterfaceWithId;
+    if (session.expiration < new Date()) {
+        await deleteSessionById(encryptedSessionID);
+        return null;
+    }
+    return {
+        ...session,
+        _id: encryptedSessionID,
+    };
+};
 
-export async function getSessionById(_id: string) {
-    return await SessionModel.findById(_id).exec() as SessionInterface;
+export async function deleteSessionById(encryptedSessionID: string) {
+    const session_id = decrypt(encryptedSessionID); 
+    return await SessionModel.findByIdAndDelete(session_id).exec() as SessionInterface;
 };
