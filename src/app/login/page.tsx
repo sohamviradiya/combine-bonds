6471dab24f7215784c4d0453cd64e8abef31fe5b
sohamviradiya <sohamviradiya2003@gmail.com>
2023-09-11 18:29:39 +0530

    fix login bugs

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -4,23 +4,23 @@ import { useAuth } from "@/context/session";
 import { Container, Typography, TextField, Button, FormControl, Paper } from "@mui/material";
 import { useState } from "react";
 import background from "public/login-background.svg";
-import { redirect } from "next/navigation";
+import { useRouter } from "next/navigation";
 
 export default function LoginPage() {
     const [user, setUser] = useState({ name: '', password: '' });
     const { login, session } = useAuth();
     const [error, setError] = useState('');
     const [info, setInfo] = useState('Remember your credentials!');
-
+    const router = useRouter();
     return (
-        <Container maxWidth="xl" sx={{ display: 'flex', flexDirection: 'column', gap: '1rem', position: 'relative', padding: '2rem', height: "100vh", textAlign: "center" }}>
+        <Container maxWidth="xl" sx={{ display: 'flex', flexDirection: 'column', gap: '1rem', position: 'relative', padding: '2rem', minHeight: "100vh", textAlign: "center" }}>
             <Background src={background} />
             <Typography variant="h1">Login</Typography>
             <Paper sx={{ display: 'flex', flexDirection: 'column', gap: '1rem', alignSelf: 'center', width: "60%", padding: "2rem" }} elevation={3}>
                 <FormControl>
                     <TextField
                         label="Username"
-                        name="fsafsd"
+                        name="name"
                         type="text"
                         required
                         value={user.name}
@@ -48,7 +48,7 @@ export default function LoginPage() {
                         login(user).then(({ session, message }) => {
                             console.log({ session, message });
                             if (session)
-                                redirect('/portfolio');
+                                router.push('/portfolio');
                             else {
                                 setInfo('Try Logging in again');
                                 setError(message);
