c754e25abae71b1b515735778e76cd29e738723c
sohamviradiya <sohamviradiya2003@gmail.com>
2023-09-11 22:09:23 +0530

    change admin method to post

--- src/app/api/admin/route.ts
+++ src/app/api/admin/route.ts
@@ -1,7 +1,7 @@
 import MainStart from "@/server/main/start.main";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(request: NextRequest) {
+export async function POST(request: NextRequest) {
     const { key } = await request.json();
     if (key !== process.env.ADMIN_KEY) return NextResponse.json({ message: "Incorrect key" }, { status: 403 });
     return NextResponse.json(await MainStart());
