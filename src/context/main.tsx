58e077b6246e36f31cf5016d2f7474f1fd5104f0
sohamviradiya <sohamviradiya2003@gmail.com>
2023-09-13 14:21:36 +0530

    remove header & footer from context

--- src/context/main.tsx
+++ src/context/main.tsx
@@ -4,8 +4,6 @@ import MaskProvider from "@/context/mask";
 import QueryProvider from "@/context/query";
 import ThemeContextProvider from "@/context/theme";
 import SessionProvider from "@/context/session";
-import Header from "@/components/header";
-import Footer from "@/components/footer";
 
 export default function MainContext({ children, }: { children: React.ReactNode }) {
     return (
@@ -14,9 +12,7 @@ export default function MainContext({ children, }: { children: React.ReactNode }
                 <ThemeContextProvider>
                     <MaskProvider>
                         <>
-                            <Header />
                             {children}
-                            <Footer />
                         </>
                     </MaskProvider>
                 </ThemeContextProvider>
