da8ebf84d8e638a44248f94f461a8b77b6adee86
sohamviradiya <sohamviradiya2003@gmail.com>
2023-09-13 14:22:13 +0530

    extend data-typography

--- src/components/data-typography.tsx
+++ src/components/data-typography.tsx
@@ -1,10 +1,10 @@
 import { Typography } from '@mui/material';
 
-export default function DataTypography({ value, unit, sign, label }: { value: number; unit: string; sign?: boolean, label?: string }) {
+export default function DataTypography({ value, unit, sign = false, label, precision = 2 }: { value: number; unit: string; sign?: boolean, label?: string, precision?: number }) {
     return (<Typography variant="h5" sx={{
         color: value >= 0 ? 'success.main' : 'error.main',
     }}>
         <Typography variant="h5" component="span" color="white">{label ? `${label} : ` : ''}</Typography>
-        {` ${sign ? (value >= 0 ? '+' : '-') : ''} `} {Math.abs(value).toFixed(2)} {unit}
+        {` ${sign ? (value >= 0 ? '+' : '-') : ''} `} {Math.abs(value).toFixed(precision)} {unit}
     </Typography>);
 };
