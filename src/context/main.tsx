bd88cb0f21213a047928faa74f16614e760d32dd
sohamviradiya <sohamviradiya2003@gmail.com>
2023-10-05 14:48:39 +0530

    remove mask

--- src/context/main.tsx
+++ src/context/main.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import MaskProvider from "@/context/mask";
 import QueryProvider from "@/context/query";
 import ThemeContextProvider from "@/context/theme";
 import SessionProvider from "@/context/session";
@@ -10,11 +9,9 @@ export default function MainContext({ children, }: { children: React.ReactNode }
         <SessionProvider>
             <QueryProvider>
                 <ThemeContextProvider>
-                    <MaskProvider>
                         <>
                             {children}
                         </>
-                    </MaskProvider>
                 </ThemeContextProvider>
             </QueryProvider>
         </SessionProvider>
