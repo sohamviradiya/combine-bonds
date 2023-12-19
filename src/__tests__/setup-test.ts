0a3bf1962cf510ccb84936f896f85ed75c6e3afe
sohamviradiya <sohamviradiya2003@gmail.com>
2023-12-19 19:30:36 +0530

    add mongodb memory server

---
+++ src/__tests__/setup-test.ts
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+import { MongoMemoryServer } from "mongodb-memory-server";
+
+export async function initializeMongoServer() {
+    const mongoServer = await MongoMemoryServer.create();
+    const mongoUri = mongoServer.getUri();
+
+    mongoose.connect(mongoUri);
+
+    mongoose.connection.on("error", e => {
+        if (e.message.code === "ETIMEDOUT") {
+            console.log(e);
+            mongoose.connect(mongoUri);
+        }
+        console.log(e);
+    });
+
+    mongoose.connection.once("open", () => {
+        console.log(`MongoDB successfully connected to ${mongoUri}`);
+    });
+}
+
+export async function closeMongoServer() {
+    mongoose.disconnect();
+};
